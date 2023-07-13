9cbb0bbbecc560cc6191eaf3b4c8531f6313cf70
Dean Minnie <dminnie33@gmail.com>
2023-07-13 11:01:36 +0200

    Setting up integration tests

--- src/test/java/com/mtech/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/mtech/orderservice/OrderServiceApplicationTests.java
@@ -1,10 +1,46 @@
 package com.mtech.orderservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mtech.orderservice.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
+@AutoConfigureMockMvc
 class OrderServiceApplicationTests {
+
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest")
+            .withDatabaseName("mysql-integration-tests-db")
+            .withUsername("sa")
+            .withPassword("sa");
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", mySQLContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
 
     @Test
     void contextLoads() {
